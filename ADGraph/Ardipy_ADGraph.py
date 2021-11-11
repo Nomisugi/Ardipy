b6b73333ff459776444c50c49c3f7e13d48243bf
Nomisugi <nomi.sugi.7015@gmail.com>
2021-11-11 19:09:20 +0900

    ADGraphC³

--- ADGraph/Ardipy_ADGraph.py
+++ ADGraph/Ardipy_ADGraph.py
@@ -17,92 +17,26 @@ import binascii
 import re
 import time
 import tkinter as tk
+import tkinter.ttk as ttk
 sys.path.append('../')
 from Ardipy_Driver import Ardipy
 sys.path.append('../Tool')
 from IOLogWindow import *
-
-from matplotlib.backends.backend_tkagg import (
-    FigureCanvasTkAgg, NavigationToolbar2Tk)
-from matplotlib.figure import Figure
-import matplotlib.animation as animation
-import numpy as np
+from Ardipy_Frame  import Ardipy_Frame
+from SelectableGraph import *
 
 PlotNum = 80
 
-Ardipy_ADGraph = "1.0"
-Initial_File = "ADCGraph.ini"
+Ardipy_ADGraph = "1.2"
 
-class OtherException(Exception):
+class AdgraphException(Exception):
     pass
 
-class GraphWindow(tk.LabelFrame):
+class Control_Frame(Ardipy_Frame):
     def __init__(self, master):
-        super().__init__(master,relief='ridge')
-        self.pack()
-        fig = Figure(figsize=(10.0, 8.0))  #graph size
-        canvas = FigureCanvasTkAgg(fig, master=master)
-        self.run_flag = False
-        self.view_flags = [True,True,True,True,True,True,True,True]
-        self.x = np.arange(0, 40, 0.5)  # x軸(固定の値)
-        self.y1 = np.zeros(PlotNum)
-        self.y2 = np.zeros(PlotNum)
-        self.l = np.arange(0, 5, 0.1)   # 表示期間(FuncAnimationで指定する関数の引数になる)
-        plt = fig.add_subplot(111)
-        plt.set_position([0.07,0.05,0.9,0.9])
-        plt.set_ylim([-0.2,5.5])
-        ani = animation.FuncAnimation(fig, self.animate, self.l,
-                                      init_func=self.init, interval=5, blit=True,
-        )
-        self.line1, = plt.plot(self.y1)
-        self.line2, = plt.plot(self.y1)
-        self.line1.visible = False
-        canvas.get_tk_widget().pack()
-
-        #補助線
-        for i in range(6):
-            plt.hlines([i], 0, PlotNum, "blue", linestyles='dashed',linewidth = 0.5, alpha = 0.5)
-            plt.hlines([i+0.5], 0, PlotNum, "blue", linestyles='dashed',linewidth = 0.5, alpha = 0.2)
-        
-    def animate(self, i):
-        if self.run_flag:
-            self.line1.set_ydata(self.y1)  # update the data.
-            self.line2.set_ydata(self.y2)  # update the data.
-        else:
-            self.line1.visible = False
-        return self.line1, self.line2,
-
-    def init(self):  # only required for blitting to give a clean slate.
-        self.line1.set_ydata(self.y1)
-        self.line2.set_ydata(self.y2)
-        return self.line1,
-
-    def setValues(self, y1, y2):
-        self.y1 = np.insert(self.y1, 0, y1)
-        self.y1 = np.delete(self.y1, PlotNum)
-        self.y2 = np.insert(self.y2, 0, y2)
-        self.y2 = np.delete(self.y2, PlotNum)
-
-    def start(self):
-        if self.run_flag:
-            self.run_flag = False
-        else:
-            self.run_flag = True
-
-    def view(self, num):
-        print(num)
-        if self.view_flags[num]:
-            self.view_flags[num] = False
-            self.line1.visible = False
-        else:
-            self.view_flags[num] = True
-            self.line1.visible = False
-
-class Control_Frame(tk.LabelFrame):
-    def __init__(self, master):
-        super().__init__(master, relief='ridge')
-        log = ''
-        self.ardipy = Ardipy(log)
+        self.log = None
+        self.ardipy = Ardipy(self.log)
+        super().__init__(master, self.ardipy)
 
         #LogWindow
         self.log_win = tk.Toplevel()
@@ -120,47 +54,30 @@ class Control_Frame(tk.LabelFrame):
             self.log_win.deiconify()
         menubar.add_command(label='LogWindow', command=open_log)
         
-        #Arduino Control
-        arduino_frame = tk.LabelFrame(master, text= "Arduino",relief = 'groove')
-        arduino_txt = tk.Entry(arduino_frame)
-        arduino_txt.insert(0, "disconnect")
-        arduino_txt.pack(side='left')
-        arduino_frame.pack(side = 'top', fill = 'x')
-        def connect_on_click():
-            try:
-                str = self.ardipy.autoConnect()
-            except:
-                print("connect error")
-            arduino_txt.delete(0, tk.END)
-            arduino_txt.insert(0, "connect:"+str)
-            self.ardipy.reset()
-        connect_button = tk.Button(arduino_frame, text="Connect", command=connect_on_click)
-        connect_button.pack(side = 'left')
-        def disconnect_on_click():
-            self.ardipy.disconnect()
-            arduino_txt.delete(0, tk.END)                
-            arduino_txt.insert(0, "disconnect")
-        disconnect_button = tk.Button(arduino_frame, text="Disconnect", command=disconnect_on_click)
-        disconnect_button.pack(side = 'left')
-
         #Control Frame
         control_frame = tk.LabelFrame(master, text= "Control",relief = 'groove')
         start_button = tk.Button(control_frame, text="START/STOP", command=lambda:self.graph.start())
         start_button.pack(side = 'left')
         control_frame.pack(side = 'top', fill = 'x')
-#        sc1 = SimpleCheck(control_frame, text="AD0", flag=True,
-#                        command=lambda:self.graph.view(0))
-#        sc1.pack(side = 'left')
 
-        self.graph = GraphWindow(master)
+        graph_list = ["AD0", "AD1", "AD2", "AD3", "AD4", "AD5"]
+        graph_frame = ttk.Frame(master)
+        self.gf = SelectableGraph(graph_frame, graph_list)
+        graph_frame.pack(side = 'right', fill = 'both')
         self.update()
 
     def update(self):
+        vals = []
         if self.ardipy.isConnect():
-            self.graph.setValues((self.ardipy.adRead(0) * (5/1024)),
-                                 (self.ardipy.adRead(1))* (5/1024))
-        self.master.after(50, self.update)
-
+            vals.append(self.ardipy.adRead(0) * (5/1024))
+            vals.append(self.ardipy.adRead(1) * (5/1024))
+            vals.append(self.ardipy.adRead(2) * (5/1024))
+            vals.append(self.ardipy.adRead(3) * (5/1024))
+            vals.append(self.ardipy.adRead(4) * (5/1024))
+            vals.append(self.ardipy.adRead(5) * (5/1024))
+            self.gf.setValues( vals )
+        self.master.after(100, self.update)
+        return
     
 
 if __name__ == "__main__":
---
+++ Tool/SelectableGraph.py
@@ -0,0 +1,161 @@
+import numpy as np
+import tkinter as tk
+import tkinter.ttk as ttk
+import math
+
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.figure import Figure
+import matplotlib.animation as animation
+
+class GraphFrame(ttk.Frame):
+    def __init__(self, master, graph_num):
+        super().__init__(master)
+        #Graph view
+        self.fig = Figure()
+        canvas = FigureCanvasTkAgg(self.fig, master=master)
+        self.x = np.arange(0, 10, 0.1)
+        self.y_datas = []
+        for y in range(graph_num):
+            self.y_datas.append( np.zeros(500) )
+        l = np.arange(0, 10, 0.01)
+        self.plt = self.fig.add_subplot(111)
+        self.plt.set_ylim([-2, 2])
+        self.plt.set_position([0.07, 0.05, 0.9, 0.9])
+        self.lines = []
+        self.interval = 20
+        for y in range(graph_num):
+            yp, = self.plt.plot(self.y_datas[y])
+            self.lines.append( yp, )
+
+        self.ani = animation.FuncAnimation(self.fig, self.animate, l,
+                                      interval=self.interval, blit=True,
+        )
+        canvas.get_tk_widget().pack()
+
+    def animate(self, i):
+        count = 0
+        self.ani.event_source.interval = self.interval
+        for line in self.lines:
+            line.set_ydata(self.y_datas[count])
+            count+=1
+        return self.lines[0:]
+
+    def setGraph_Y_Range(self, y_min, y_max):
+        self.plt.set_position([0.07, 0.05, 0.9, 0.9])
+        self.plt.set_ylim([y_max, y_min])
+        self.fig.canvas.draw()
+
+    def setGraph_X_Range(self, x):
+        self.plt.set_position([0.07, 0.05, 0.9, 0.9])
+        self.plt.set_xlim([0, x])
+        self.fig.canvas.draw()
+        
+    def setInterval(self, interval):
+        print (interval)
+        self.interval = interval
+
+
+    def setValues(self, vals):
+        count = 0
+        for val in vals:
+            self.y_datas[count] = np.append(self.y_datas[count], val)
+            self.y_datas[count] = np.delete(self.y_datas[count], 0)
+            count+=1
+
+    def visible(self, opts):
+        count = 0
+        for opt in opts:
+            self.lines[count].set_visible(opt.get())
+            count+=1
+
+class SelectableGraph(ttk.Frame):
+    def __init__(self, master, graph_list):
+        super().__init__(master)
+        self.pack()
+
+        self.graph_num = len(graph_list)
+
+        control_frame = ttk.Frame(self)
+        control_frame.pack(side = 'left', anchor = tk.N)        
+
+        select_frame = ttk.LabelFrame(control_frame, text= "line Select",relief = 'groove')
+        select_frame.pack(side = 'top', anchor = tk.N)
+
+        range_frame = ttk.LabelFrame(control_frame, text= "Y Axis Range",relief = 'groove')
+        range_frame.pack(side = 'top', anchor = tk.N)
+        y_min_frame = ttk.Frame(range_frame)
+        y_min = ttk.Label(y_min_frame, text='y min').pack(side = 'left')
+        self.y_min_text = tk.StringVar()
+        ttk.Entry(y_min_frame, textvariable=self.y_min_text, width=10).pack(side = 'right')
+        self.y_min_text.set("-2")
+        y_min_frame.pack(side = 'top')
+        y_max_frame = ttk.Frame(range_frame)        
+        y_max = ttk.Label(y_max_frame, text='y max').pack(side = 'left')
+        self.y_max_text = tk.StringVar()
+        ttk.Entry(y_max_frame, textvariable=self.y_max_text, width=10).pack(side = 'right')
+        self.y_max_text.set("2")
+        y_max_frame.pack(side = 'top')
+        ttk.Button(range_frame, text = 'Submit', command=self.on_y_range_submit).pack()
+
+        interval_frame =  ttk.LabelFrame(control_frame, text= "Plot Number",relief = 'groove')
+        interval_frame.pack(side = 'top', anchor = tk.N)
+        self.x_text = tk.StringVar()
+        ttk.Entry(interval_frame, textvariable=self.x_text, width=8).pack(side = 'left')
+        self.x_text.set("100")
+        ttk.Button(interval_frame, text = 'set', command=self.on_x_range_submit, width =5).pack()
+        
+        
+        def on_check():
+            self.gf.visible(opts)
+
+        opts = [tk.BooleanVar() for _ in range(self.graph_num)]
+        for item in opts:
+            item.set(True)
+        for i in range(self.graph_num):
+            ttk.Checkbutton(select_frame, text = graph_list[i],
+                            variable = opts[i], command=on_check).pack()        
+
+        graph_frame = ttk.Frame(self)
+        self.gf =GraphFrame(graph_frame, self.graph_num)
+        graph_frame.pack(side = 'right', fill = 'x')
+
+    def setValues(self, vals):
+        self.gf.setValues(vals)
+
+    def on_y_range_submit(self):
+        self.gf.setGraph_Y_Range( int(self.y_max_text.get()), int(self.y_min_text.get()))
+
+    def on_x_range_submit(self):
+        self.gf.setGraph_X_Range( int(self.x_text.get()) )
+
+
+class RealtimeUpdateGraph(ttk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.test_count = 0
+        self.master = master
+
+        #Selectable lines
+        graph_list = ["sin", "cos", "tan"]
+        graph_frame = ttk.Frame(master)
+        self.gf =SelectableGraph(graph_frame, graph_list)
+        graph_frame.pack(side = 'right', fill = 'x')
+
+        self.update()
+
+    def update(self):
+        vals = []
+        vals.append( math.sin(math.radians(10*self.test_count)))
+        vals.append( math.cos(math.radians(10*self.test_count)))
+        vals.append( math.tan(math.radians(10*self.test_count)))                
+        self.gf.setValues( vals )
+        self.test_count+=1
+        self.master.after(100, self.update)
+
+
+if __name__ == "__main__":
+    win = tk.Tk()
+    gh = RealtimeUpdateGraph(win)
+    win.mainloop()
+    
